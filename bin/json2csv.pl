be27cd2cedc0ce71edaed45e4ae05bbf87e82563
holly <emperor.kurt@gmail.com>
2021-05-06 23:04:11 +0900

    add utility scripts. csv2json.pl and json2csv.pl

---
+++ bin/json2csv.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use utf8;
+use feature qw(say);
+use Encode;
+use Encode::Guess qw(euc-jp shiftjis 7bit-jis);
+use FindBin qw($Bin $Script);
+use FundScraping::Util qw(:all);
+use Getopt::Long qw(:config posix_default no_ignore_case gnu_compat);
+
+our $VERSION = '1.0';
+our $AUTHOR  = 'holly';
+
+
+## Parse options
+my %opts;
+
+GetOptions(\%opts, qw(
+	headers=s
+ ));
+
+if (!defined($opts{headers})) {
+	exit 1;
+}
+
+my $data = "";
+while (my $line = <STDIN>) {
+	$data .= $line;
+}
+
+my $enc = guess_encoding($data);
+if (ref($enc)) {
+	$data = $enc->decode($data);
+} else {
+	$data = Encode::decode("guess", $data);
+}
+
+binmode STDOUT, ":utf8";
+my $ref = json2ref($data);
+say ref2csv($ref, [split /,/, $opts{headers}]);
